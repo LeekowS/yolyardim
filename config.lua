6f07bc5a6ddced609d6202ebb8fe508251b3d878
LeekowS <66480633+LeekowS@users.noreply.github.com>
2020-08-02 14:33:11 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,42 @@
+companyName = "Yol Yardım"       
+companyIcon = "CHAR_LS_CUSTOMS" 
+spawnRadius = 100              
+drivingStyle = 786603           
+simplerRepair = false           
+repairComsticDamage = false     
+flipVehicle = false             
+
+
+mechPeds = {
+
+
+                [1] = {name = "Mekanik Mülayim", icon = "CHAR_MP_MECHANIC", model = "S_M_Y_DockWork_01", vehicle = 'UtilliTruck3', colour = 111, 
+                                ['lines'] = {
+                                        "Yenisi kadar olmasada idare eder.",
+                                        "Umarım yakın zamanda aracını ciddi bir tamire verirsin.",
+                                        "İşte şimdi yolda kalmazsın.",
+                                        "Elimden bu kadarı geldi.",
+                                        "Ben bu işin ustasıyım.",
+                                        "Biraz sihir kullanmam gerekti ama oldu.",
+                                        "Ne diyebilirim ki ? Ben bu işte anlıyorum.",
+                                        "Kolaydı!",
+                                        "Umarım araba senindir. Masrafı çok...",
+                                        "Tamir edemediğim tek şey sevgilimin kalbi.",
+                                        "Onarım tamam. Emniyet kemerini bağlı tut ve biraz yavaş sür !",
+                                        "İki orta bir sade, bu ustaya müsade.",}},
+
+                [2] = {name = "Mekanik Sıtkı", icon = "CHAR_MP_BIKER_MECHANIC", model = "S_M_Y_Construct_01", vehicle = 'BobcatXL', colour = 118, 
+                                ['lines'] = {
+                                        "Yeehaw, işte şimdi çıtır bir kız kadar taze oldu.",
+                                        "Elimden gelen bu. Umarım elimden gelmeyecek bir duruma düşmezsin.",
+                                        "Bu motor ölmüş. Yakın zamanda tamirhaneye getir.",
+                                        "Yapabileceğim herşeyi yaptım ama bazen tecavüz kaçınılmazsa zevk almaya bakacaksın.",
+                                        "Umarım kasaplar artık ehliyet yerine sadece et satıyorlardır!",
+                                        "Ne diyelim gittiği kadar gider gitmediği kadarsa kader.",
+                                        "Araba tamir ediyorum mutlu sanıyorlar.",
+                                        "Hızlı ve öfkeli çok masraflı bir filmdi bence sende o kadar para yoktur.",
+                                        "Kız gibi oldu !",
+                                        "Hmm... Yakın zamanda hurdalığa uğra.",
+                                        "İşte şimdi oldu."}},
+
+                }
